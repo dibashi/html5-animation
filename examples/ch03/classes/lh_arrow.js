ac1a52bdb1af0a3015e0beb62a8185602545c22b
dibashi <dibashi1989@163.com>
2018-12-10 00:26:51 +0800

    lh_demo1

---
+++ examples/ch03/classes/lh_arrow.js
@@ -0,0 +1,36 @@
+
+function LH_arrow() {
+    this.x = 0;
+    this.y = 0;
+    this.color = '#ffff00';
+    this.rotation = 0;
+}
+
+LH_arrow.prototype.draw = function(context) {
+    context.save();
+
+    context.translate(this.x,this.y);
+    context.rotate(this.rotation);
+    context.lineWidth = 2;
+    context.fillStyle = this.color;
+
+    context.beginPath();
+    context.moveTo(-50,-25);
+    context.lineTo(0,-25);
+    context.lineTo(0,-50);
+
+    context.lineTo(50,0);
+
+    context.lineTo(0,50);
+    context.lineTo(0,25);
+    context.lineTo(-50,25);
+
+    //context.lineTo(-50,-25);
+
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+
+    context.restore();
+}
